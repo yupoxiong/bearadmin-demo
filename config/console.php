01787d658eeff2c96593e5ecb2271c89be6f2bcf
于破熊 <8553151@qq.com>
2022-03-03 14:27:38 +0800

    更新基础框架

--- config/console.php
+++ config/console.php
@@ -2,10 +2,27 @@
 // +----------------------------------------------------------------------
 // | 控制台配置
 // +----------------------------------------------------------------------
+use app\command\Chat;
+use app\command\AdminReset;
+use app\command\GenerateAppKey;
+use app\command\GenerateJwtKey;
+use app\command\InitEnv;
+use app\command\Test;
+
 return [
     // 指令定义
     'commands' => [
-        'admin:reset' => 'app\command\AdminReset',
-        'chat' => 'app\command\Chat',
+        // 重置管理员密码
+        'admin:reset'      => AdminReset::class,
+        // 聊天
+        'chat'             => Chat::class,
+        // 测试指令
+        'test'             => Test::class,
+        // 初始化env文件
+        'init:env'         => InitEnv::class,
+        // 生成新的app_key
+        'generate:app_key' => GenerateAppKey::class,
+        // 生成新的jwt_key
+        'generate:jwt_key' => GenerateJwtKey::class,
     ],
 ];
